62a91e9501c762203929957853dc35d061420672
AhmedMekkawy27 <mekkawy39@gmail.com>
2024-07-03 01:42:13 +0300

    correcting todos number

--- app/components/Todos.tsx
+++ app/components/Todos.tsx
@@ -11,7 +11,7 @@ const Todos = ({ todo, onclick, rem }: { todo: ITodo['todos'], onclick?: (event:
             <div className={`w-6 h-6 flex justify-center items-center bg-white rounded-full ${todo.status && 'bg-check-gradient'} border-[1px] border-black/[0.2] dark:border-white/[0.2]`} onClick={onclick}>
                 <TiTick className='text-white cursor-pointer ' />
             </div>
-            <p className={`dark:text-white text-[#514f64] cursor-pointer flex-1 mx-5 overflow-clip py-5 text-sm md:text-lg tracking-wider ${todo.status && 'line-through opacity-40'}`} onClick={onclick}>{todo.title}</p>
+            <p className={`dark:text-white text-[#514f64] cursor-pointer flex-1 mx-5 overflow-hidden py-5 text-sm md:text-lg tracking-wider ${todo.status && 'line-through opacity-40'}`} onClick={onclick}>{todo.title}</p>
             <button onClick={rem}>
                 <TfiClose className='text-[#514f64] w-6 h-6 dark:text-white/[0.2] cursor-pointer' />
             </button>
