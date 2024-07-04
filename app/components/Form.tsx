7f7a2f56ea5e8dad993699cce028581d6225e99b
AhmedMekkawy27 <mekkawy39@gmail.com>
2024-07-04 21:39:51 +0300

    placeholder

--- app/components/Form.tsx
+++ app/components/Form.tsx
@@ -17,7 +17,7 @@ const Form = () => {
     }
     return (
         <form className="w-full h-[10%] shadow-2xl">
-            <input value={input} onChange={(e) => { setInput(e.target.value) }} type="text" className="w-full h-full ps-[72px] py-10 focus:outline-none rounded-md dark:bg-[#25273c] bg-white shadow-2xl dark:text-white text-lg" />
+            <input value={input} placeholder="Create A New Todo" onChange={(e) => { setInput(e.target.value) }} type="text" className="w-full h-full ps-[72px] py-10 focus:outline-none rounded-md dark:bg-[#25273c] bg-white shadow-2xl dark:text-white text-lg" />
             <button onClick={addTodo} className="hidden">
                 Add
             </button>
