7a096cf3392eccae746fa90e5236d6d735887b0a
AhmedMekkawy27 <mekkawy39@gmail.com>
2024-07-07 02:38:11 +0300

    phone height

--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,13 @@ export default function Home() {
     <main className="h-screen relative ">
       <div className="absolute z-10 left-0 top-0 w-full h-[35%] bg-light-mobile dark:bg-dark-mobile md:bg-light-desktop dark:md:bg-dark-desktop bg-cover" />
       <div className="absolute bottom-0 left-0 w-screen h-screen dark:bg-[#181824] bg-[#fafafa]" />
-      <div className="flex justify-evenly mx-auto items-center flex-col h-[750px] w-[85vw] md:h-[750px] md:w-[620px] z-30 h relative md:top-10 ">
-        <div className="flex justify-between items-center w-full h-[8%]">
+      <div className="flex py-8 justify-start md:justify-evenly mx-auto items-center flex-col h-[750px] w-[85vw] md:h-[750px] md:w-[620px] z-30 h relative md:top-10 ">
+        <div className="flex justify-between items-center w-full h-[8%] mb-7">
           <h1 className="text-white text-6xl tracking-widest">Todo</h1>
           <ThemeSwitch />
         </div>
         <Form />
-        <div className="w-full h-[55%] md:h-[65%] dark:bg-[#25273c] bg-white rounded-md shadow-2xl flex flex-col justify-between">
+        <div className="w-full mt-8 h-[55%] md:h-[65%] dark:bg-[#25273c] bg-white rounded-md shadow-2xl flex flex-col justify-between">
           <TodosBody />
           <Footer />
         </div>
