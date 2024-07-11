58d188c01fcbb895415a3e483f44b5d8d5174a1c
AhmedMekkawy27 <mekkawy39@gmail.com>
2024-07-11 22:08:12 +0300

    login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 "use client"
 import { useRouter } from "next/navigation"
 import { useState, useEffect, useRef } from "react"
@@ -21,12 +22,12 @@ const Login = () => {
         }
     }, [])
     return (
-        <div className="flex justify-center items-center bg-black/[0.2] w-screen h-screen">
+        <div className="flex justify-center items-center dark:bg-[#181824] w-screen h-screen">
             <div className="w-[50%] h-[50%] flex justify-center items-center">
                 <form action="" className="flex flex-col justify-between gap-8">
                     <p ref={err} className="text-red-600 hidden">Username must be letters followed by numbers</p>
                     <input type="text" value={username} onChange={e => setUsername(e.target.value)} placeholder="Username" className="outline-none border-gray-700 border-2 rounded-md px-4 py-2" />
-                    <button type="submit" onClick={handleSubmit} className="bg-slate-900 py-2 rounded-md">Submit</button>
+                    <button type="submit" onClick={handleSubmit} className="bg-slate-900 text-white py-2 rounded-md">Submit</button>
                 </form>
             </div>
         </div>
